ac5ff0509a6bf9a72529895151bb306102c08b5f
aarelaid <angela.aarelaid3@gmail.com>
2024-06-13 19:12:55 +0300

    Second commit

--- cypress/e2e/new_issue_comments.cy.js
+++ cypress/e2e/new_issue_comments.cy.js
@@ -1,61 +1,61 @@
 // Important data:
 const getIssueDetailsModal = () =>
   cy.get('[data-testid="modal:issue-details"]');
+const message1 = "Are you sure you want to delete this comment?";
+const message2 = "Once you delete, it's gone for good.";
+const inputComment = 'textarea[placeholder="Add a comment..."]';
+const issueComment = '[data-testid="issue-comment"]';
+const listIssue = '[data-testid="list-issue"]';
 
 function addAComment(comment) {
   cy.contains("Add a comment...").click();
-  cy.get('textarea[placeholder="Add a comment..."]').type(comment);
+  cy.get(inputComment).type(comment);
   cy.contains("button", "Save").click().should("not.exist");
   cy.contains("Add a comment...").should("exist");
-  cy.get('[data-testid="issue-comment"]').should("contain", comment);
+  cy.get(issueComment).should("contain", comment);
 }
 
 function editAComment(previousComment, newComment) {
   getIssueDetailsModal().within(() => {
-    cy.get('[data-testid="issue-comment"]')
+    cy.get(issueComment)
       .should("contain", previousComment)
       .parent()
       .within(() => {
         cy.contains("Edit").click();
       });
 
-    cy.get('textarea[placeholder="Add a comment..."]').clear().type(newComment);
+    cy.get(inputComment).clear().type(newComment);
 
     cy.contains("button", "Save").click().should("not.exist");
-    cy.get('[data-testid="issue-comment"]').should("contain", newComment);
+    cy.get(issueComment).should("contain", newComment);
   });
 }
 
-function deleteComment(comment) {
+function deleteAComment(comment) {
   getIssueDetailsModal().within(() => {
     cy.get('[data-testid="issue-comment"]').should("contain", comment).parent();
     cy.contains("Delete").click();
   });
 
   cy.get('[data-testid="modal:confirm"]')
-    .contains("Are you sure you want to delete this comment?")
+    .contains(message1)
     .should("be.visible")
     .parent()
-    .contains("Once you delete, it's gone for good.")
+    .contains(message2)
     .should("be.visible")
     .parent()
     .contains("button", "Delete comment")
     .should("be.visible")
     .click();
 
-  cy.get('[data-testid="issue-comment"]').should("not.contain", comment);
+  cy.get(issueComment).should("not.contain", comment);
 }
 
 describe("Issue comments creating, editing and deleting", () => {
   beforeEach(() => {
-    cy.visit("/");
-    cy.url()
-      .should("eq", `${Cypress.env("baseUrl")}project/board`)
-      .then((url) => {
-        cy.visit(url + "/board");
-        cy.contains("This is an issue of type: Task.").click();
-        getIssueDetailsModal().should("be.visible");
-      });
+    cy.visit("https://jira.ivorreic.com/");
+    cy.get(listIssue).first().click();
+    getIssueDetailsModal().should("be.visible");
   });
 
   it("Add a comment", () => {
@@ -76,8 +76,8 @@ describe("Issue comments creating, editing and deleting", () => {
 
   it("Delete a comment", () => {
     const comment = "This is a task to be done";
-    
+
     addAComment(comment);
-    deleteComment(comment);
+    deleteAComment(comment);
   });
 });
